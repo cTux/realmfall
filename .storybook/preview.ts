9077138046665ba45dcd310f2249871360f657d7
Viacheslav Miroshko <ccctux@gmail.com>
2026-04-19 14:00:14 +0300

    fix: load storybook preview after i18n

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,13 +2,14 @@ import type { Preview } from '@storybook/react-vite';
 import { loadI18n } from '../src/i18n';
 import '../src/styles/base.scss';
 import './preview.scss';
-import { storybookPreviewDecorator } from '../src/ui/components/storybook/storybookPreviewDecorator';
+
+await loadI18n();
+const { storybookPreviewDecorator } = await import(
+  '../src/ui/components/storybook/storybookPreviewDecorator'
+);
 
 const preview: Preview = {
   decorators: [storybookPreviewDecorator],
-  async beforeAll() {
-    await loadI18n();
-  },
   parameters: {
     layout: 'fullscreen',
     controls: {
